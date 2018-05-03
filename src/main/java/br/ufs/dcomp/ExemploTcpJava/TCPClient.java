94a37139e82edd31e86d63efb7a812bdf26f537e
Oscalmi Porto Freitas <oscalmi.freitas@dcomp.ufs.br>
2018-05-03 00:01:25 +0000

    meu primeiro commit no repositório

--- src/main/java/br/ufs/dcomp/ExemploTcpJava/TCPClient.java
+++ src/main/java/br/ufs/dcomp/ExemploTcpJava/TCPClient.java
@@ -8,7 +8,7 @@ import java.io.*;
 public class TCPClient{
     public static void main(String[] args){
         try {
-            System.out.print("[ Conectando com o Servidor TCP    ..................  ");
+            System.out.print("[ Cliente: Conectando com o Servidor TCP    ..................  ");
             Socket sock = new Socket("127.0.0.1", 3300);
             System.out.println("[OK] ]");
             
@@ -17,9 +17,23 @@ public class TCPClient{
             String msg = "Olá, DCOMP!!!";
             byte[] buf = msg.getBytes(); // Obtendo a respresntação em bytes da mensagem
 
-            System.out.print("[ Enviando mensagem    ..............................  ");
+            System.out.print("[ Cliente: Enviando mensagem    ..............................  ");
             os.write(buf);
             System.out.println("[OK] ]");
+            
+            //ALTERADO
+            byte[] outBuf = new byte[20]; // buffer de recepção
+            
+            System.out.print("[ Cliente: Aguardando recebimento de mensagem   ..............  ");
+            is.read(outBuf); // Operação bloqueante (aguardando chegada de dados)
+            System.out.println("[OK] ]");
+            
+            String outMsg = new String(outBuf); // Mapeando vetor de bytes recebido para String
+            
+            System.out.println(" Cliente: Mensagem recebida: "+ outMsg);
+            
+            
+            
         }catch(Exception e){System.out.println(e);}    
         System.out.println("[ FIM ]");
     }
